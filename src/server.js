23fc8322a602f8bb3df1380702228a7f12661663
valdeilson <vautronica@gmail.com>
2019-09-07 14:46:51 -0300

    instalando e configurando o CORS

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
-
-
+const cors = require('cors');
 const routes = require('./routes');
 
 const server = express();
+mongoose.connect('mongodb+srv://omnistack8:omnistack8@cluster0-rsqkf.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
-mongoose.connect('mongodb+srv://omnistack8:omnistack8@cluster0-rsqkf.mongodb.net/omnistack8?retryWrites=true&w=majority', {useNewUrlParser: true});
-
-
+server.use(cors());
 server.use(express.json());
 
 server.use(routes);
