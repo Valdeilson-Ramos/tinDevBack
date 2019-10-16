3aeb35ac4c85135d34279b56d2050c8a6343ce4e
valdeilson <vautronica@gmail.com>
2019-10-16 00:04:51 -0300

    commit final - add socket io

--- src/server.js
+++ src/server.js
@@ -3,7 +3,19 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
 
-const server = express();
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user } = socket.handshake.query;
+
+  console.log(user, socket.id);
+
+  connectedUsers[user] = socket.id;
+});
 mongoose.connect(
   "mongodb+srv://omnistack8:omnistack8@cluster0-rsqkf.mongodb.net/omnistack8?retryWrites=true&w=majority",
   {
@@ -11,7 +23,14 @@ mongoose.connect(
   }
 );
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+  return next();
+});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 server.listen(3333);
+
+//parei no minuto 23:05
